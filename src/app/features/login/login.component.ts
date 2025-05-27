07eacdd625c4122acef2d4e44bf80b6b75ba3062
gilberto.junior <gilberto.junior@ciss.com.br>
2025-05-27 18:07:08 -0300

    :sparkles: feat: Tela de login

---
+++ src/app/features/login/login.component.ts
@@ -0,0 +1,26 @@
+import {Component, inject} from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout'
+import {NgClass, NgStyle} from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    NgClass,
+    NgStyle
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  isLargeScreen: boolean = false;
+  breakpointObserver = inject(BreakpointObserver);
+
+  constructor() {
+    this.breakpointObserver.observe([Breakpoints.Medium, Breakpoints.Large, Breakpoints.XLarge])
+    .subscribe(result => {
+      this.isLargeScreen = result.matches;
+    });
+  }
+
+}
