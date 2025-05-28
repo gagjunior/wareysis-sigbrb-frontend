ec9d09d21b198891e2ac7886fcfa6665eb5986de
gilberto.junior <gilberto.junior@ciss.com.br>
2025-05-28 14:39:02 -0300

    :sparkles: feat: Tela de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,21 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: '',
-        redirectTo: 'login',
-        pathMatch: 'full'
-    },
-    {
-        path: 'login',
-        loadComponent: () => import('./features/login/login.component').then(m => m.LoginComponent)
-    }
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./features/login/login.component').then(m => m.LoginComponent)
+  },
+  {
+    path: 'registro',
+    loadComponent: () => import('./features/registro/registro.component').then(m => m.RegistroComponent)
+  },
+  {
+    path: 'onde-estamos',
+    loadComponent: () => import('./features/onde-estamos/onde-estamos.component').then(m => m.OndeEstamosComponent)
+  }
 ];
--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,12 +1,16 @@
 import {Component, inject} from '@angular/core';
 import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout'
 import {NgClass, NgStyle} from '@angular/common';
+import {RouterLink} from '@angular/router';
+import {RodapeComponent} from '../../shared/rodape/rodape.component';
 
 @Component({
   selector: 'app-login',
   imports: [
     NgClass,
-    NgStyle
+    NgStyle,
+    RouterLink,
+    RodapeComponent
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
