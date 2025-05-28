43ec9b25cddec5b5e89594987880977315503419
gilberto.junior <gilberto.junior@ciss.com.br>
2025-05-28 15:07:20 -0300

    :sparkles: feat: Componente onde-estamos

--- src/app/features/onde-estamos/onde-estamos.component.ts
+++ src/app/features/onde-estamos/onde-estamos.component.ts
@@ -1,8 +1,11 @@
 import {Component} from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-onde-estamos',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './onde-estamos.component.html',
   styleUrl: './onde-estamos.component.css'
 })
