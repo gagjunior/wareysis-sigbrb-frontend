2c75abc0ef4cbaf928a0a7a2a969b900260200f2
gilberto.junior <gilberto.junior@ciss.com.br>
2025-05-28 14:39:56 -0300

    :sparkles: feat: Onde estamos

---
+++ src/app/features/onde-estamos/onde-estamos.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-onde-estamos',
+  imports: [],
+  templateUrl: './onde-estamos.component.html',
+  styleUrl: './onde-estamos.component.css'
+})
+export class OndeEstamosComponent {
+
+  emailBarbearia :string = 'contato@nomedaloja.com';
+
+}
